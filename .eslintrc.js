07f27b6432e924d4c786cb66adc425db0af44a32
Anton <anton98767@gmail.com>
2024-01-30 16:59:48 +0300

    update axios version

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,10 +1,14 @@
 module.exports = {
   root: true,
   parserOptions: {
-    parser: 'babel-eslint',
+    parser: '@babel/eslint-parser',
     sourceType: 'module'
   },
   extends: [
     '@nuxtjs'
-  ]
+  ],
+  rules: {
+    'no-undef': 0,
+    'no-console': 0
+  }
 }
